4e83d268971f65d2e383c17b71e8d271b02b74d4
Adedamola Ogundipe <adedamolaogundipe11@gmail.com>
2024-11-22 14:48:39 +0100

    Add files via upload

---
+++ kafka/my-python-client-lesson/kafka-consumer.py
@@ -0,0 +1,4 @@
+from kafka import KafkaConsumer
+consumer = KafkaConsumer('quickstart-events',bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest')
+for msg in consumer: 
+    print (msg.value)
